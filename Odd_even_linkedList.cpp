5ddf3cf304b96e8b4229cc7016af4d13a14c466d
mittaldivya <35059727+mittaldivya01@users.noreply.github.com>
2020-05-17 17:22:23 +0530

    Odd Even Linked List in O(1) space & O(n) time
    Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
    
    You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
    Input: 1->2->3->4->5->NULL
    Output: 1->3->5->2->4->NULL

---
+++ Odd_even_linkedList.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;+
+ 
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        
+        if(head == nullptr)
+            return head;
+        ListNode * even=head->next;
+        ListNode * odd=head;
+        ListNode * evenhead=even;
+        while(even!=nullptr && even->next != nullptr)
+        {
+         odd->next=even->next;
+         odd=odd->next;
+         even->next=odd->next;
+         even=even->next;
+        }
+        odd->next=evenhead;
+        return head;
+    }
+};
