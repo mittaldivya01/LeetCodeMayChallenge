cf2773a267d1194c8773bfe43f246c61bbca327f
mittaldivya <35059727+mittaldivya01@users.noreply.github.com>
2020-05-23 16:18:44 +0530

    Update Interval_List_Intersections.cpp

--- Interval_List_Intersections.cpp
+++ Interval_List_Intersections.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
-      vector<vector<int>> res;  
-        int sa=A.size();
-        int sb=B.size();
-        for(int i=0;i<sa;++i)
-        {
-            for(int j=0;j<sb;++j){
-                //break if A and B do not overlap 
-                if(A[i][1]<B[j][0] && A[i][0]<B[j][0])
-                    break;
-                //continue if A dont overlap with B  
-                if(B[j][0]<A[i][0] && B[j][1]<A[i][0] )
-                    continue;
-                //get max lowwer bound and min higher bound 
-                res.push_back({max(B[j][0], A[i][0]),min(A[i][1],B[j][1])});                   
-            }
-       }
-      return res;  
+vector<vector> intervalIntersection(vector<vector>& A, vector<vector>& B) {
+    vector<vector> res;
+    int sa=A.size();
+    int sb=B.size();
+    int i=0,j=0,lo,hi;
+    while(i<sa,j<sb)
+    {
+        //get max lowwer and min higher interval pointer
+        lo= max(B[j][0], A[i][0]);
+        hi=min(A[i][1],B[j][1]);
+        if(lo<hi)
+            res.push_back({lo,hi});
+        //update pointer of disjoint lists as if A's endpoint > B's endpoint and vice versa
+        if(A[i][1]>B[j][1])
+            i++;
+        else
+            j++;
     }
+    return res;
+}
 };
